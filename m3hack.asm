e6b7e904166b221133ce564e5032e461b6d955df
JumpmanFR <jumpman@jumpman.fr>
2021-03-05 15:58:13 +0100

    Refonte du sprite CASTING avec la police d’origine + retouches police crédits

--- m3hack.asm
+++ m3hack.asm
@@ -1660,7 +1660,9 @@ org $9FAA3C4; incbin gfx_bazaar2_[c].bin
 
 // Insert graphic for the CAST sprite
 org $9658CB0; incbin gfx_cast.bin
-org $96599B0; incbin gfx_castleft.bin
+org $9659AB0; incbin gfx_castleft.bin
+org $1A87871; db $00
+org $1A87875; db $E0,$70,$61
 
 
 // Insert up/down stats
